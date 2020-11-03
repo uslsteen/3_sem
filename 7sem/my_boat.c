5713ee0696d1973a8e96624305eec3429d49941c
uslsteen <shurygin0171@gmail.com>
2020-11-03 04:42:14 +0300

    not ready vers

--- 7sem/my_boat.c
+++ 7sem/my_boat.c
@@ -1,132 +1,189 @@
-#include "my_sem.h"
-
 //! Heroes of this program
 
-void Captain(int num_of_trips, int sem_id)
+void Captain(int sem_id, int num_of_trips, int cap_of_stair, int cap_of_boat)
 {
+    //! Init of Semaphors
+    V_oper(sem_id, STAIR_DOWN, 1);
+    V_oper(sem_id, START_OF_TRIP, 1);
+    V_oper(sem_id, END_OF_TRIP, 1);
+    V_oper(sem_id, NUM_ON_STAIR, cap_of_stair);
+    //! End of initialisation sems
+
     //! Boat preparing
     printf("Hello, I'm your captain!\n");
     printf("Trap is going down!\n");
 
+    P_oper(sem_id, STAIR_DOWN, 1);
+
     for (int i = 0; i < num_of_trips; ++i)
     {
-      printf("Welcome to the boat!\n");
+        if (i == num_of_trips - 1)
+            V_oper(sem_id, LAST_TRIP, 1);
 
-      //! Passengers go to boat
-      //! Captain waiting for passangers
+        printf("Welcome to the boat!\n");
 
-      printf("Enough! Trap is going up!\n");
-      printf("Let's start our trip!\n");
+        Z_oper(sem_id, NUM_ON_STAIR);
+        //! Captain is waiting for people
+        Z_oper(sem_id, NUM_ON_BOAT);
 
-      printf("        *** Two hours later ***        \n");
+        printf("Captain is waiting for the stair to be removed\n");
+        printf("Stair is going up!\n");
+        V_oper(sem_id, STAIR_DOWN, 1);
 
-      printf("Ladies ang gentelments, our trip is over!\nGo away from my boat!\n");
+        P_oper(sem_id, START_OF_TRIP, 1);
+        printf("Let's start our trip!\n");
 
-      //! Reloading of boat
+        printf("        *** Two hours later ***        \n");
+        P_oper(sem_id, END_OF_TRIP, 1);
 
+        printf("Ladies ang gentelments, our trip is over!\nGo away from my boat!\n");
+
+        //! Captain down the stair
+        P_oper(sem_id, STAIR_DOWN, 1);
+
+        Z_oper(sem_id, NUM_ON_BOAT);
+
+        Z_oper(sem_id, NUM_ON_STAIR);
+
+        Z_oper(sem_id, NUM_ON_COAST);
+
+        //! Captain is preparing boat for new quant of pepople
+        printf("Wow, the boat is finally empty. I have finished another journey.\n");
+
+        V_oper(sem_id, START_OF_TRIP, 1);
+        V_oper(sem_id, END_OF_TRIP, 1);
+
+        P_oper(sem_id, NUM_ON_COAST, cap_of_boat);
+
+
+        if (i == num_of_trips - 1)
+            break;
     }
 
-
+    printf("Captain go away!\n");
 }
 
 void Passengers(int sem_id, int index)
 {
-//  printf("I'm passenger number %d, and I'm waiting for the trip on trap", index);
-
-//  printf("I'm passenger number %d and I'm going to the boat!\n");
-
-//  printf("I'm passenger number %d and I'm going ")
-
-  printf("Hello, I'm passenger number %d", index);
-
-  while(true)
-  {
-    //! Waiting for NULL
-    Z_oper(sem_id, STAIR_DOWN);
-
-    //! Passenger[index] go to elevator
-    //! Took sem
-    P_oper(sem_id, NUM_OF_STAIR, -1);
-    printf("Passenger number %d have gone on the stair!\n", index);
-
-    //! Passenger[index] go to boat
-    //! Gave away sem
-    V_oper(sem_id, NUM_OF_STAIR, 1);
-    printf("Passenger number %d have gone to the boat", index);
-
-    //! Took sem for passenger on boat
-    P_oper(sem_id, NUM_ON_BOAT, -1);
+        printf("Hello, I'm passenger number %d\n", index);
+        //! Waiting for stair(NULL)
+        Z_oper(sem_id, STAIR_DOWN);
 
 
-    //! Passenger[index] waiting for the begging of his trip
-    Z_oper(sem_id, START_OF_TRIP);
+        P_oper(sem_id, NUM_ON_STAIR, 1);
+        printf("Passenger number %d stepped in the stair!\n", index);
 
-    //! Passenger[index] waiting for the ending of his trip
-    Z_oper(sem_id, END_OF_TRIP);
+        P_oper(sem_id, NUM_ON_BOAT, 1);
+        printf("Passenger nuber %d stepped into the boat!\n", index);
 
-    //! Passenger[index] waiting for the down of stair
-    Z_oper(sem_id, STAIR_DOWN);
+        //! Passenger[index] waiting for the begging of his trip
+        Z_oper(sem_id, START_OF_TRIP);
 
-    //! Gave away sem for passenger on boat
-    V_oper(sem_id, NUM_ON_BOAT, 1);
+        //! Passenger[index] waiting for the ending of his trip
+        Z_oper(sem_id, END_OF_TRIP);
 
-    //! Passenger[index] go to the ladder
-    P_oper(sem_id, NUM_OF_STAIR, -1);
-    printf("Passenger number %d have gone on the stair\n", index);
+        //! Passenger[index] waiting for the down of stair
+        Z_oper(sem_id, STAIR_DOWN);
 
-    //! Passenger[index] go to island
-    V_oper(sem_id, NUM_OF_STAIR, 1);
-    printf("Passenger number %d have gone on the island\n", index);
+        //! Passenger[index] go away from boat
+        V_oper(sem_id, NUM_ON_BOAT, 1);
+        printf("Passenger number %d have gone on the stair\n", index);
+
+        //! Passenger[index] go away from stair
+        V_oper(sem_id, NUM_ON_STAIR, 1);
+        printf("Passenger number %d have gone on the island\n", index);
+
+        P_oper(sem_id, NUM_ON_COAST, 1);
 
 
-    P_oper(sem_id, GO_TO_COAST, -1);
-  }
-
-  printf("Passenger numver %d glad for trip on the boat!\n", index);
-
+        printf("Passenger number %d is glad for trip on the boat!\n", index);
 }
+
+
 
 int main(int argc, char** argv)
 {
+    int num_of_pass = 5,
+            num_of_trips = 1,
+            cap_of_boat = 5,
+            cap_of_stair = 5;
+
+  if (argc >= 5)
+  {
   //! Getting arguments of terminal
-  int num_of_pass = strtol(argv[1], NULL, 10)
-      num_of_trips = strtol(argv[2], NULL, 10),
-      cap_of_boat = strtol(argv[3], NULL, 10)
-      cap_of_trap = strto(argv[4], NULL, 10);
-
-  pid_t captains_pid = fork();
-
-  int sem_id = semget(IPC_PRIVATE, num_of_semaphors, 0700);
-
-  if (captains_pid == 0)
-  {
-    //! Add processing cap_of_boat and num_of_pass
-    Captain(num_of_trips, sem_id);
-
-    return 0;
-  }
-  else
-  {
-    perror("Captains pid not zero!\n");
-    return ERROR;
+    num_of_pass = strtol(argv[1], NULL, 10),
+    num_of_trips = strtol(argv[2], NULL, 10),
+    cap_of_boat = strtol(argv[3], NULL, 10),
+    cap_of_stair = strtol(argv[4], NULL, 10);
   }
 
-  for (int i = 0; i < num_of_pass; ++i)
-  {
-    pid_t passngrs_pid = fork();
 
-    if (passngrs_pid)
+    int sem_id = semget(IPC_PRIVATE, num_of_semaphors, 0777);
+    if (sem_id < 0)
     {
-      Passengers(sem_id, i + 1);
+        perror("Semget return error!\n");
+        return ERROR;
+    }
 
-      return 0;
+    //Num_processing(cap_of_boat, num_of_pass, sem_id);
+
+    if (cap_of_boat <= num_of_pass)
+    {
+        V_oper(sem_id, NUM_ON_COAST, cap_of_boat);
+        V_oper(sem_id, NUM_ON_BOAT, cap_of_boat);
     }
     else
     {
-      perror("Passengers pid not zero!\n");
-      return ERROR;
+        V_oper(sem_id, NUM_ON_COAST, num_of_pass);
+        V_oper(sem_id, NUM_ON_BOAT, num_of_pass);
     }
-  }
 
-  return 0;
+    pid_t captains_pid = fork();
+
+    if (captains_pid < 0)
+    {
+        perror("Captains pid isnt zero!\n");
+        return ERROR;
+    }
+
+    if (captains_pid == 0)
+    {
+        if (cap_of_boat <= num_of_pass)
+            Captain(sem_id, num_of_trips, cap_of_stair, cap_of_boat);
+
+        else if (cap_of_boat > num_of_pass)
+            Captain(sem_id, num_of_trips, cap_of_stair, num_of_pass);
+
+        return 0;
+    }
+
+    for (int i = 0; i < num_of_pass; ++i)
+    {
+        pid_t passngrs_pid = fork();
+
+        if (passngrs_pid < 0)
+        {
+            perror("Passengers pid not zero!\n");
+            return ERROR;
+        }
+
+        else if (passngrs_pid == 0)
+        {
+            Passengers(sem_id, i + 1);
+            return 0;
+        }
+
+    }
+    for (int i = 0; i < num_of_pass + 1; ++i)
+    {
+        if (wait(NULL) < 0)
+        {
+            perror("wait error!\n");
+            return ERROR;
+        }
+    }
+
+    semctl(sem_id, 0, IPC_RMID);
+    return 0;
+
 }
--- 7sem/my_sem.h
+++ 7sem/my_sem.h
@@ -1,51 +1,51 @@
+/*
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
+
 #include <sys/sem.h>
+*/
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+/*
+const int num_of_people = 10;
+const int cap_of_boat = 2;
+ const int num_of_travels = 5;
+*/
 
-//* const int num_of_people = 10;
-//* const int cap_of_boat = 2;
-//* const int num_of_travels = 5;
-const int num_of_semaphors = 10;
+const int num_of_semaphors = 6;
 
 const int ERROR = -1;
 
-
-//! ?????????????????????????????????????????????????? !//
-union semun {
-    int              val;    /* Value for SETVAL */
-    struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-    unsigned short  *array;  /* Array for GETALL, SETALL */
-    struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                                (Linux-specific) */
-};
-//! ?????????????????????????????????????????????????? !//
-
-
 enum Semaphors
 {
-
+  STAIR_DOWN = 0,
+  START_OF_TRIP,
+  NUM_ON_BOAT,
+  NUM_ON_STAIR,
+  END_OF_TRIP,
+  GO_TO_COAST,
+  LAST_TRIP
 };
 
-struct sem_buf
-{
-  unsigned short sem_num;  //! semaphore number
-  short          sem_op;   //! semaphore operation
-  short          sem_flg;  //! operation flags (it will be zero in my realization)
-
-}
 
 //! Semaphors operation: p(c, 15) <=> minus 15 to semaphor c,
 //!                      v(c, 15) <=> plus 15 to semaphor c,
-//!                      z
+//!                      z waiting for NULL
 
 
 void P_oper(int sem_id, enum Semaphors sem_num, int num)
 {
-  struct sem_buf sem_struct = {sem_num, -num, 0};
+  struct sembuf sem_struct = {sem_num, -num, 0};
 
-  if (semop(sem_id, sem_struct, 1) != 0)
+  if (semop(sem_id, &sem_struct, 1) < 0)
   {
     perror("sem op return not zero in P_oper!\n");
     exit(0);
@@ -54,9 +54,9 @@ void P_oper(int sem_id, enum Semaphors sem_num, int num)
 
 void V_oper(int sem_id, enum Semaphors sem_num, int num)
 {
-  struct sem_buf sem_struct = {sem_num, num, 0};
+  struct sembuf sem_struct = {sem_num, num, 0};
 
-  if (semop(sem_id, sem_struct, 1) != 0)
+  if (semop(sem_id, &sem_struct, 1) < 0)
   {
     perror("sem op return not zero in V_oper!\n");
     exit(0);
@@ -65,9 +65,9 @@ void V_oper(int sem_id, enum Semaphors sem_num, int num)
 
 void Z_oper(int sem_id, enum Semaphors sem_num)
 {
-  struct sem_buf sem_struct = {sem_num, 0, 0};
+  struct sembuf sem_struct = {sem_num, 0, 0};
 
-  if (semop(sem_id, sem_struct, 1) != 0)
+  if (semop(sem_id, &sem_struct, 1) < 0)
   {
     perror("sem op return not zero in Z_oper!\n");
     exit(0);
