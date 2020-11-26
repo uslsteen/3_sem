cc5932ffe9b4bdd3e8bed13ef5bdc788104f9e92
uslsteen <shurygin0171@gmail.com>
2020-11-26 05:25:24 +0300

    it doesnt work now

--- semaphs_tasks/herds_man.c
+++ semaphs_tasks/herds_man.c
@@ -1,119 +1,130 @@
  #include "semaphors.h"
 
-void Herdsman(int sem_id, int index)
-{
-  printf("Hello, I'm herdsman number %.\nAnd I'm going to the top of mountain!\n");
-  Herdsman_down(sem_id, index)
 
-  printf("Hello, I'm herdsman number %.\nAnd I'm going to the top of mountain!\n");
-  Herdsman_up(sem_id, index)
-}
 
 //! Herdsman in the bottom !//
 void Herdsman_down(int sem_id, int index)
 {
-  P_oper(LOCK_UP);
+  P_oper(sem_id, LOCK_UP, 1);
   Upwait++;
 
   if (Upwait != 1)
   {
     //! Herdsman not first in queue
-    V_oper(LOCK_UP);
-    P_oper(UP_QUEUE);
-    P_oper(LOCK_UP);
+    V_oper(sem_id, LOCK_UP, 1);
+    P_oper(sem_id, UP_QUEUE, 1);
+    P_oper(sem_id, LOCK_UP, 1);
   }
   if (!Cap)
   {
-    V_oper(LOCK_UP);
+    V_oper(sem_id, LOCK_UP, 1);
     //! Оставляем стадо и идем вверх;
     //! Кладем шапку;
-    P_oper(LOCK_UP);
+    P_oper(sem_id, LOCK_UP, 1);
     Cap = 1;
-    V_oper(LOCK_UP);
-    P_oper(DOWN);
+    V_oper(sem_id, LOCK_UP, 1);
+    P_oper(sem_id, DOWN, 1);
+
     //! Идем обратно;
     //! ждем возможности входа
-    P_oper(UP);
-    P_oper(LOCK_UP);
+    P_oper(sem_id, UP, 1);
+    P_oper(sem_id, LOCK_UP, 1);
   }
 
   // формирование колонны
   Upwait--;
   if (Upwait != 0)
   {
-    V_oper(UP_QUEUE);
+    V_oper(sem_id, UP_QUEUE, 1);
     Upgo++;
-    V_oper(LOCK_UP);
+    V_oper(sem_id, LOCK_UP, 1);
   }
 
-  printf("I'm herdsman number %d and I'm going through the bom to the top!\n");
+  printf("I'm herdsman number %d and I'm going through the bom to the top!\n", index);
 
-  P_oper(LOCK_UP);
+  P_oper(sem_id, LOCK_UP, 1);
   Upgo--;
 
   if (Upgo == 0)
   { // конец прохода колонны
     Cap = 0;
-    V_oper(DOWN);
+    V_oper(sem_id, DOWN, 1);
+    printf("I'm herdsman number %d , and I went through the bom!\n", index);
   }
 
-  V_oper(LOCK_UP);
+  V_oper(sem_id, LOCK_UP, 1);
 }
 
 
 //! Herdsman at the top !//
-void Herdsman_down(int sem_id, int index)
+void Herdsman_up(int sem_id, int index)
 {
   // Проверяем, не лежит ли шапка
-  P_oper(DOWN);
-  P_oper(LOCK_DOWN);
+  P_oper(sem_id, DOWN, 1);
+  P_oper(sem_id, LOCK_DOWN, 1);
 
   // Первый в колонне блокирует вход снизу
 
   if (Downgo == 0)
   {
-    P_oper(UP);
+    P_oper(sem_id, UP, 1);
     Downgo ++;
-    V_oper(LOCK_DOWN);
-    V_oper(DOWN);
+    V_oper(sem_id, LOCK_DOWN, 1);
+    V_oper(sem_id, DOWN, 1);
   }
 
-  printf("I'm herdsman number %d and I'm going through the bom to the bottom!\n");
+  printf("I'm herdsman number %d and I'm going through the bom to the bottom!\n", index);
 
-  P_oper(LOCK_DOWN);
+  P_oper(sem_id, LOCK_DOWN, 1);
   Downgo--;
 
   // Последний в колонне разблокирует вход снизу
 
   if (Downgo == 0)
   {
-    V_oper(UP);
-    V_oper(LOCK_DOWN);
+    V_oper(sem_id, UP, 1);
+    V_oper(sem_id, LOCK_DOWN, 1);
   }
 }
 
-void Sems_processing(int sem_id, int num_of_herdmans)
+void Sems_processing(int sem_id)
 {
     //! Semaphors initialization
     //! Semaphore Lock_up = 1, Lock_down = 1;
     //! Semaphore Up_queue = 0, Up = 1, Down = 1;
-
     V_oper(sem_id, LOCK_UP, 1);
     V_oper(sem_id, LOCK_DOWN, 1);
-    V_oper(sem_id, UP_QUEUE);
+    V_oper(sem_id, UP_QUEUE, 1);
+    P_oper(sem_id, UP_QUEUE, 1);
     V_oper(sem_id, UP, 1);
     V_oper(sem_id, DOWN, 1);
 }
 
-int main()
+void Herdsman(int sem_id, int index)
 {
+  printf("Hello, I'm herdsman number %d.\nAnd I'm going to the top of mountain!\n", index);
+  Herdsman_down(sem_id, index);
+
+  printf("Hello, I'm herdsman number %d.\nAnd I'm going to the top of mountain!\n", index);
+  Herdsman_up(sem_id, index);
+
+  printf("I'm herdsman number %d and I'm going away!\n", index);
+}
+
+int main(int argc, char** argv)
+{
+  pid_t herdmans_pid = 0;
+  int num_of_herdmans = 0;
+
+  if (argc < 2)
+    num_of_herdmans = 5;
+  else
+    num_of_herdmans = atoi(argv[1]);
+
   char print_buf[BUFSIZ];
   setvbuf(stdout, print_buf, _IOLBF, BUFSIZ);
-  pid_t herdmans_pid = 0;
 
-  int num_of_herdmans = 5;
-
-  int sem_i= semget(IPC_PRIVATE, num_of_semaphors, 0777);
+  int sem_id = semget(IPC_PRIVATE, num_of_semaphors, 0777);
 
   if (sem_id < 0)
   {
@@ -121,7 +132,7 @@ int main()
     return ERROR;
   }
 
-  Sems_processing(sem_id, num_of_herdmans);
+  Sems_processing(sem_id);
 
   if ((herdmans_pid = fork()) < 0)
   {
@@ -131,12 +142,24 @@ int main()
 
   if (herdmans_pid == 0)
   {
-    for (int i = 0; i < num_of_herdmans; ++i)
+    for (int i = 1; i != num_of_herdmans; ++i)
     {
       Herdsman(sem_id, i);
       return 0;
     }
   }
+
+  for (int i = 0; i < num_of_herdmans; ++i)
+  {
+    if (wait(NULL) < 0)
+    {
+      perror("wait return error!\n");
+      return ERROR;
+    }
+  }
+
+  semctl(sem_id, 0, IPC_RMID);
+
 
   return 0;
 }
