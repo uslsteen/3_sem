9210615d93e951140bc403b2fd107de93e9c8791
uslsteen <shurygin0171@gmail.com>
2020-11-30 19:30:28 +0300

    some fixes

--- 7sem/MY_BOAT/new_boat/semaphors.h
+++ 7sem/MY_BOAT/new_boat/semaphors.h
@@ -1,46 +1,27 @@
-//
-// Created by anon on 29.11.2020.
-//
-
-#ifndef VORUSH_CODE_SEMS_H
-#define VORUSH_CODE_SEMS_H
-
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/ipc.h>
 #include <sys/sem.h>
-#include <sys/wait.h>
-
-
 #include <errno.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <sys/msg.h>
 
 
+const int num_of_semaphors = 8;
 const int ERROR = -1;
 
 
 
-int Up_wait = 0;  //! Variable for num of waiting herdsmans
-int Up_go = 0;    //! Var for num of going up herdsmans
-int Down_go = 0;  //! Var for num of going down herdsmans
-int Cap = 0;         //! Flag for hat on the top enter
-
-
 enum Semaphors
 {
-    LOCK_UP = 0, //! Sem for going up herdsmans
-    LOCK_DOWN,   //! Sem for going down herdsmans
-    UP_QUEUE,    //! Sem for locking herdsman if queue after bottom enter in bom
-    UP,          //! Sem for locking bottom enter in bom
-    DOWN         //! Sem for locking top enter in bom
-};
-
-struct Message_t
-{
-    long value; /* тип сообщения, должен быть > 0 */
+    STAIR_DOWN = 0,
+    START_OF_TRIP,
+    NUM_ON_BOAT,
+    NUM_ON_STAIR,
+    END_OF_TRIP,
+    NUM_ON_COAST,
+    LAST_TRIP
 };
 
 
@@ -72,8 +53,6 @@ void V_oper(int sem_id, enum Semaphors sem_num, short int num)
     }
 }
 
-
-/*
 void Z_oper(int sem_id, enum Semaphors sem_num)
 {
     struct sembuf sem_struct = {sem_num, 0, 0};
@@ -84,7 +63,3 @@ void Z_oper(int sem_id, enum Semaphors sem_num)
         exit(0);
     }
 }
-*/
-
-
-#endif //VORUSH_CODE_SEMS_H
