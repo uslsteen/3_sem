23e5900875f92c902508916cc396fd8ecdcae28c
uslsteen <shurygin0171@gmail.com>
2020-12-12 14:34:07 +0300

    upd

--- Final_work/Final_work.c
+++ Final_work/Final_work.c
@@ -4,6 +4,20 @@
 
 #include <time.h>
 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdbool.h>
+#include <assert.h>
+
+
+int Err_proc(char* err_string)
+{
+  perror(err_string);
+  return errno;
+}
 
 void Print_time(struct timespec start_clock, struct timespec final_clock)
 {
@@ -15,8 +29,34 @@ void Print_time(struct timespec start_clock, struct timespec final_clock)
   printf("Process time in msecs : %lf\n", time);
 }
 
+void My_time(int argc, char** argv)
+{
+  struct timespec start_clock = {0, 0}, final_clock = {0, 0};
+
+  if (clock_gettime(CLOCK_REALTIME, &start_clock) != 0)
+    Err_proc("Error in clock_gettime, start clock!\n");
+
+  pid_t pid = fork();
+
+  if (pid == 0)
+  {
+
+  }
+
+  
+}
+
 
 int main(int argc, char** argv)
 {
-  
+  if (argc < 2)
+  {
+    perror("Lack arguments!\n");
+    exit(0);
+  }
+  else
+  {
+    My_time(argc, argv);
+    return 0;
+  }
 }
